5f977f4ff3839648977bc0b5c0df39cc8219fe6c
Jatin Bhutka <jatinbhutka@gmail.com>
2019-12-22 21:28:41 -0800

    Update 6.3 maxDepthOfN_ary_Tree.py

--- 6.3 maxDepthOfN_ary_Tree.py
+++ 6.3 maxDepthOfN_ary_Tree.py
@@ -47,3 +47,23 @@ class Solution:
         else:
             height = [self.maxDepth(c) for c in root.children]
             return max(height) + 1
+
+          
+          
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        # Base case
+        if root == None:
+            return 0
+        # Depth level of the tree
+        depth = 0
+        
+        # Loops through children array
+        for child in root.children:
+            # Compares current depth of depth with a new level of depth 
+            # Sets the biggest value to variable depth
+            depth = max(depth, self.maxDepth(child))
+        
+        # As going deeper into the tree increases depth by 1
+        #print ('root ' + str(root.val) + ' depth ' + str(depth + 1))
+        return depth + 1 
