c5a3bee46eb0c30f229096bd11a30b6a2527d9de
Jatin Bhutka <jatinbhutka@gmail.com>
2019-12-25 17:25:30 -0800

    Create 0.4 Threading in python.py

---
+++ 0.4 Threading in python.py
@@ -0,0 +1,7 @@
+# https://www.geeksforgeeks.org/multithreading-python-set-1/
+
+# https://www.geeksforgeeks.org/multithreading-in-python-set-2-synchronization/
+
+"""
+Print Even-Odd Number using Treading
+"""
