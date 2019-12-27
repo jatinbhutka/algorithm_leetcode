a344959ad8043d5ede4e8b64b7c06f7f68f6979a
Jatin Bhutka <jatinbhutka@gmail.com>
2019-12-27 00:29:04 -0800

    Create 8.2 Ngram.py

---
+++ 8.2 Ngram.py
@@ -0,0 +1,37 @@
+# SAP Interview
+
+s = "Mary had a little lamb"
+N = 3
+
+#R = [‘Mary had a’, ‘had a little’, ‘a little lamb’]
+
+
+
+def nGram(s, n):
+    r = []
+    s = s.split()
+    for i in range(len(s)-n + 1):
+        a = s[i: (i+n)]
+        #print a
+        a = ' '.join(a)
+        r.append(a)
+        #del a
+    return r
+
+
+print(nGram(s, N))
+
+
+def nGram1(s, n):
+    s = s.split()
+    iterator = len(s) - n + 1
+    r = []
+    for i in range(iterator):
+        a = s[i:i+n]
+        a = ' '.join(a)
+        r.append(a)
+        a = []
+    return r
+
+print(nGram1(s, N))
+    
