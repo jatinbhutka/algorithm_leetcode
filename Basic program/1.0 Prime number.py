b2254c823cd12fee6f80b0f41f1d45a4e7669deb
Jatin Kumar <jbhutka@cisco.com>
2020-01-08 13:18:10 -0800

    added prime

---
+++ Basic program/1.0 Prime number.py
@@ -0,0 +1,12 @@
+# prime Number:
+
+def checkPrime(n):
+    if (n <= 1):
+        return False
+    
+    for i in range(2,n):
+        if n % i == 0:
+            return False
+    return True
+
+print(checkPrime(-1))
