a5eb39233ec7013d9055cadf8ec3ffec3e1e18e8
Jatin Bhutka <jatinbhutka@gmail.com>
2020-01-09 15:39:22 -0800

    Update 2.1 Merge Two Sorted Lists.py

--- 2.1 Merge Two Sorted Lists.py
+++ 2.1 Merge Two Sorted Lists.py
@@ -88,4 +88,57 @@ class Solution:
         else:
             newhead.next = l2
         return rethead
+    
+    
+# Method 3:
+
+# Method 2:
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 is None:
+            return l2
+        if l2 is None:
+            return l1
         
+        if l1.val <= l2.val:
+            newhead = l1
+            l1 = l1.next
+        else:
+            newhead = l2
+            l2 = l2.next
+        rethead = newhead
+        
+        while l1 and l2:
+            if l1.val <= l2.val:
+                newhead.next = l1
+                l1 = l1.next
+            else:
+                newhead.next = l2
+                l2 = l2.next
+            newhead = newhead.next
+            
+        newhead.next = l1 if l1 is not None else l2
+        return rethead
+    
+    
+# Method 4:
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        # maintain an unchanging reference to node ahead of the return node.
+        prehead = ListNode(-1)
+
+        prev = prehead
+        while l1 and l2:
+            if l1.val <= l2.val:
+                prev.next = l1
+                l1 = l1.next
+            else:
+                prev.next = l2
+                l2 = l2.next            
+            prev = prev.next
+
+        # exactly one of l1 and l2 can be non-null at this point, so connect
+        # the non-null list to the end of the merged list.
+        prev.next = l1 if l1 is not None else l2
+
+        return prehead.next
