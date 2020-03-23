4ee0cd88f743bd3b87c8c03afa6a65cb5f4a9736
Jatin Bhutka <jatinbhutka@gmail.com>
2020-03-23 00:33:41 -0700

    Update 6. Moving avg from data stream.py

--- Microsoft/6. Moving avg from data stream.py
+++ Microsoft/6. Moving avg from data stream.py
@@ -54,3 +54,53 @@ class MovingAverage:
 # Your MovingAverage object will be instantiated and called as such:
 # obj = MovingAverage(size)
 # param_1 = obj.next(val)
+
+
+
+#Method 3:
+"""
+What's need in our data structure?
+
+Size of the moving window
+queue for moving window (first in first out)
+length, to determine when we are in the window where the queue is growing
+total, the current total for the moving queue
+We have two distinct modes: growing queue and full size queue
+
+Growing
+
+Append the next value
+Calculate total
+Increase length
+Return Moving Average
+Full Size
+
+We pop/remove element 0,
+Append the next value
+Calculate total by subtracting element 0 and adding new value. Prevents us from having to sum the whole queue
+Return Moving Average
+
+"""
+
+class MovingAverage:
+
+    def __init__(self, size: int):
+        """
+        Initialize your data structure here.
+        """
+        self.size:int = size
+        self.queue:int = []
+        self.length:int = 0
+        self.total:int = 0
+        
+    def next(self, val: int) -> float:
+        if self.length < self.size:
+            self.queue.append(val)
+            self.total += val
+            self.length += 1
+            return self.total/self.length
+        else:
+            subtract = self.queue.pop(0)
+            self.queue.append(val)
+            self.total = self.total - subtract + val
+            return self.total/self.size
