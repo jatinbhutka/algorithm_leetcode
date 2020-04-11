4b89e60ce1eadda5f8afe747a8f83b7777c868b1
Jatin Bhutka <jatinbhutka@gmail.com>
2020-04-11 01:44:40 -0700

    Update 1299. Replace Elements with Greatest Element on Right Side.py

--- Amazon/1299. Replace Elements with Greatest Element on Right Side.py
+++ Amazon/1299. Replace Elements with Greatest Element on Right Side.py
@@ -7,3 +7,24 @@ class Solution:
             if temp > large:
                 large = temp
         return arr
+
+    
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        for i in range(len(arr)-1):
+            l = arr[i+1]
+            for j in range(i+1, len(arr)):
+                if l<arr[j]:
+                    l = arr[j]
+            arr[i] = l
+        arr[-1] = -1   
+        return arr
+    
+    
+#List Compri:
+
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        k = [max(arr[i+1:])for i in range(len(arr)-1)]
+        k.append(-1)
+        return k
