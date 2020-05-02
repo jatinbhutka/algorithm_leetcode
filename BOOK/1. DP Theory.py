f8f653ae7082b6e9e152c758cbdd40f33d482030
Jatin Bhutka <jatinbhutka@gmail.com>
2020-05-02 13:35:01 -0700

    Update 1. DP Theory.py

--- BOOK/1. DP Theory.py
+++ BOOK/1. DP Theory.py
@@ -2,8 +2,15 @@
 
 # https://www.geeksforgeeks.org/overlapping-subproblems-property-in-dynamic-programming-dp-1/
 # Dynamic Programming: 
+"""
+1) Overlapping Subproblems:
+Like Divide and Conquer, Dynamic Programming combines solutions to sub-problems. Dynamic Programming is mainly used when solutions of same subproblems are needed again and again. In dynamic programming, computed solutions to subproblems are stored in a table so that these don’t have to be recomputed. So Dynamic Programming is not useful when there are no common (overlapping) subproblems because there is no point storing the solutions if they are not needed again. For example, Binary Search doesn’t have common subproblems. If we take an example of following recursive program for Fibonacci Numbers, there are many subproblems which are solved again and again.
 
+There are following two different ways to store the values so that these values can be reused:
+a) Memoization (Top Down)
+b) Tabulation (Bottom Up)
 
+"""
 """
 
 
@@ -93,6 +100,8 @@ def fibBU(n):
 print(fibBU(n))
     
 """
+  Both Tabulated and Memoized store the solutions of subproblems. In Memoized version, table is filled on demand while in Tabulated version, starting from the first entry, all entries are filled one by one. Unlike the Tabulated version, all entries of the lookup table are not necessarily filled in Memoized version. For example, Memoized solution of the LCS problem doesn’t necessarily fill all entries.
+
     Time taken by Recursion method is much more than the two Dynamic Programming 
     techniques mentioned above – Memoization and Tabulation!
 """
