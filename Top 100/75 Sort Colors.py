e410c3b0fada8542643893f567c974d6ce0ea3fd
Jatin Bhutka <jatinbhutka@gmail.com>
2020-05-15 22:51:13 -0700

    Update 75 Sort Colors.py

--- Top 100/75 Sort Colors.py
+++ Top 100/75 Sort Colors.py
@@ -7,7 +7,6 @@
 # Time Complexity: O(N) - Two pass.
 # Space Complexity: O(1) 
 
-
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
@@ -39,10 +38,67 @@ class Solution:
             
 # Method 2: One Pass
 
-# Steps:
-  # 1. 
-  # 2.
-  # 3.
+        # One Passs solution:
+        # Steps:
+        #   1. 
+        #   2. 
+        #   3. If temp < 1: 
+        #   4. If temp > 1: 
+"""
+We are classifying the array into four groups: red, white, unclassified, and blue. Initially we group all elements into unclassified. We iterate from the beginning as long as the white pointer is less than the blue pointer.
+
+If the white pointer is red (nums[white] == 0), we swap with the red pointer and move both white and red pointer forward. If the pointer is white (nums[white] == 1), the element is already in correct place, so we don't have to swap, just move the white pointer forward. If the white pointer is blue, we swap with the latest unclassified element.
+"""
+# Time Complexity: O(N) 
+# Space Complexity: O(1) 
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        r = 0
+        w = 0
+        b = len(nums)-1
+        while w <= b:
+            
+            if nums[w] < 1:
+                nums[w],nums[r] = nums[r], nums[w]
+                w += 1
+                r += 1     
+            elif nums[w] == 1:
+                w += 1
+            else:
+                nums[w], nums[b] = nums[b], nums[w]
+                b -= 1
+
+ 
+# Method 3:  No swap, no count  
+
+        # One Passs solution:
+        # Steps:
+        #   1. 
+        #   2. 
+        #   3. If temp < 1: 
+        #   4. If temp > 1: 
   
 # Time Complexity: O(N) 
-# Space Complexity: O(1)        
+# Space Complexity: O(1) 
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = 0
+        j = 0
+        n = len(nums)
+        for k in range(n):
+            temp = nums[k]
+            nums[k] = 2  
+            if temp < 2:
+                nums[j] = 1
+                j += 1
+            if temp == 0:
+                nums[i] = 0
+                i += 1                
