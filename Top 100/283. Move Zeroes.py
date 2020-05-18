140b1f0d8f34f1928882409df091a35530e3473b
Jatin Bhutka <jatinbhutka@gmail.com>
2020-05-17 19:12:46 -0700

    Update 283. Move Zeroes.py

--- Top 100/283. Move Zeroes.py
+++ Top 100/283. Move Zeroes.py
@@ -61,3 +61,16 @@ class Solution:
             nums[i] = 0
         return nums
 
+      
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        i = 0
+        for j in range(len(nums)):
+            if nums[j] != 0:
+                nums[j], nums[i] = nums[i], nums[j]
+                i += 1
+        return nums      
