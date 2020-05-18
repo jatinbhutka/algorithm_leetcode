719e79223894bd82cdf8d2c4082acd047934a848
Jatin Bhutka <jatinbhutka@gmail.com>
2020-05-17 20:01:30 -0700

    Update 34. Find First and Last Position of Element in Sorted Array.py

--- Top 100/34. Find First and Last Position of Element in Sorted Array.py
+++ Top 100/34. Find First and Last Position of Element in Sorted Array.py
@@ -57,3 +57,36 @@ class Solution:
 
   # Time complexity : O(Log N) - This brute-force approach examines each of the n elements of nums exactly twice, so the overall runtime is linear.
   # Space complexity : O(1) - The linear scan method allocates a fixed-size array and a few integers, so it has a constant-size memory footprint.
+
+  
+  class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        if len(nums) < 1:
+            return -1,-1
+        
+        start = 0
+        end = len(nums) - 1
+        
+        while start < end:
+            mid = start + (end - start)//2
+            if nums[mid] < target:
+                start = mid +1 
+            else:
+                end = mid
+        print(start)
+        if nums[start] != target:
+            return [-1,-1]
+        print(end)
+        first = start
+        
+        start = first
+        end = len(nums) - 1
+        while start < end:
+            mid = (start+end)//2 + 1
+            if nums[mid] == target:
+                start = mid
+            else:
+                end = mid - 1
+                
+        return [first, start]
+  
