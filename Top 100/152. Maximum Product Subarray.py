015244c054df869a46907183d26a1877f557b4a2
Jatin Bhutka <jatinbhutka@gmail.com>
2020-05-29 01:14:18 -0700

    Update 152. Maximum Product Subarray.py

--- Top 100/152. Maximum Product Subarray.py
+++ Top 100/152. Maximum Product Subarray.py
@@ -43,3 +43,18 @@ class Solution:
             final_max = max(curr_max,final_max)
             
         return final_max
+
+    
+    
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        
+        # Time: O(N)
+        # Space: O(N)
+        
+        A= nums
+        B = A[::-1]
+        for i in range(1, len(A)):
+            A[i] *= A[i - 1] or 1
+            B[i] *= B[i - 1] or 1
+        return max(A + B)    
